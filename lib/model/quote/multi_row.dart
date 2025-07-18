53c5588b36e82feb314abca33d68802361ec5129
yewei <1076369504@qq.com>
2025-07-18 18:02:53 +0800

    update

---
+++ lib/model/quote/multi_row.dart
@@ -0,0 +1,21 @@
+class MultiRowData {
+  List<List<Widget>> rows;  // 所有行的组件
+  List<int> order;          // 排序顺序索引
+
+  MultiRowData(this.rows, {List<int>? initialOrder})
+      : order = initialOrder ?? List.generate(rows.first.length, (i) => i);
+
+  // 获取按当前顺序排列的行
+  List<Widget> getOrderedRow(int rowIndex) {
+    return order.map((index) => rows[rowIndex][index]).toList();
+  }
+
+  // 更新排序顺序
+  void reorder(int oldIndex, int newIndex) {
+    if (oldIndex < newIndex) {
+      newIndex -= 1;
+    }
+    final item = order.removeAt(oldIndex);
+    order.insert(newIndex, item);
+  }
+}
